5a8af9d67ee3ae90ddba67af65f07b5dbd7e728f
Ali Kamel <alikamel2004@hotmail.com>
2020-07-03 13:06:45 +0100

    this is committing important files from prior iterations

--- algorithm.py
+++ algorithm.py
@@ -1,45 +1,77 @@
-#this is an attempt at creating a simple Q-Learning environment to predict stock prices of Apple
-import pandas as pd
+# this is an attempt at creating a simple Q-Learning environment to predict stock prices of Apple
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras as keras
 from sklearn.model_selection import train_test_split
-import random 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.layers import Dense
 
-
-dset = pd.read_csv("aapl/dset.csv").iloc[:,1:]
-
-closes = dset["close"].values
-closes = np.append(closes, np.NaN)
-closes = closes[1:]
-dset["Future Closes"] = closes
-dset = dset.dropna()
-x = dset.iloc[:,:-1].values
-y = closes[:len(closes)-1]
-scX = StandardScaler()
-x = scX.fit_transform(x)
-scY = StandardScaler()
-y = scY.fit_transform(y.reshape(-1,1))
-
-
-xTrain,xTest,yTrain,yTest = train_test_split(x,y, test_size=0.2, random_state=42)
-
-
-
-actionTable = ["UP", "DOWN"]
-qTable = np.zeros([len(dset), len(actionTable)])
-
-alpha, gamma, epsilon = 0.1, 0.6, 0.1
-rewards = 0
-penalties = 0
-
-for i in range(0,len(xTrain)):
-    action = random.choice(actionTable)
-    result = yTrain[i]
-    beginning = xTrain[i][2]
-    if action == "UP" and result > beginning or action=="DOWN" and result < beginning:
-        rewards += 1
+dset = pd.read_csv("aapl/dset.csv").iloc[:, 1:]
+rsi = pd.read_csv("aapl/rsi.csv").iloc[:, 1:]
+lengths = [len(dset), len(rsi)]
+idx = min(lengths)
+dset, rsi = dset[:idx], rsi[:idx]
+rsiVals = []
+for index, row in rsi.iterrows():
+    RSI = row["RSI"]
+    if RSI < 30:
+        data = [1, 0, 0]
+    elif RSI > 70:
+        data = [0, 0, 1]
     else:
-        penalties+=1
-print(f"Rewards: {rewards}")
-print(f"Penalties: {penalties}")
+        data = [0, 0, 0]
+    rsiVals.append(data)
 
+rsiVals = pd.DataFrame(rsiVals, columns={"LOW", "MID", "HIGH"})
+dset = pd.concat([dset, rsiVals], axis=1)
+closes = np.append(dset["close"].values, np.NaN)
+closes = closes[1:-1]
+
+opens = np.append(dset["open"].values, np.NaN)
+opens = opens[1:-1]
+dset = dset[:-1]
+x = dset[:-1]
+polyReg = PolynomialFeatures(degree=4)
+x = polyReg.fit_transform(x)
+y = []
+for i in range(0, len(closes) - 1):
+    if closes[i] > opens[i]:
+        y.append("UP")
+    elif closes[i] < opens[i] or closes[i] == opens[i]:
+        y.append("DOWN")
+
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2, random_state=42)
+
+scX = StandardScaler()
+xTrain = scX.fit_transform(xTrain)
+xTest = scX.fit_transform(xTest)
+
+model = tf.keras.Sequential()
+model = keras.Sequential([Dense(16, activation="tanh"),
+                          Dense(16, activation="tanh"),
+                          Dense(1, activation="tanh")])
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+model.fit(xTrain, yTrain, batch_size=32, epochs=1000)
+pred = model.predict(xTest)
+roundPred = []
+
+for i in pred:
+    avg = sum(i) / len(i)
+
+    if avg >= 0.5:
+        val = 1
+    elif avg < 0.5:
+        val = 0
+    roundPred.append(val)
+correct = 0
+for i in range(0, len(roundPred)):
+    if roundPred[i] == yTest[i]:
+        correct += 1
+
+print(f"Percentage Correct: {(correct / len(roundPred) * 100)}")
--- downloader.py
+++ downloader.py
@@ -1,11 +1,14 @@
-import string
-import random
-import requests as rq
 import os
-def randomString(length = 6):
-    alphas = list(string.ascii_letters) + list(range(1,10))
-    rString =""
-    for i in range(0,6):
+import random
+import string
+
+import requests as rq
+
+
+def randomString(length=6):
+    alphas = list(string.ascii_letters) + list(range(1, 10))
+    rString = ""
+    for i in range(0, 6):
         rString += str(random.choice(alphas))
     return rString
 
@@ -17,10 +20,19 @@ def download(symbol):
     if not os.path.isdir(symbol):
         os.mkdir(symbol)
 
-    #downloads intraday dataset
+    # downloads intraday dataset
     symbol = symbol.upper()
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+symbol+"&interval=5min&apikey="+randomString()+"&datatype=csv&outputsize=full"
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=5min&apikey=" + randomString() + "&datatype=csv&outputsize=full"
     response = rq.get(url).content
     file = open(os.path.join(symbol, "dset.csv"), "wb")
     file.write(response)
     file.close()
+
+    url = "https://www.alphavantage.co/query?function=RSI&symbol=" + symbol + "&interval=5min&time_period=20&series_type=closes&apikey=" + randomString() + "&datatype=csv&outputsize=full"
+    response = rq.get(url).content
+    file = open(os.path.join(symbol, "rsi.csv"), "wb")
+    file.write(response)
+    file.close()
+
+
+download("aapl")
